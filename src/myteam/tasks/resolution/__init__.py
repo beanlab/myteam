904b7e5c952fc6e644c10cd68dc624bfda2b2a20
Gordon Bean <gbean@cs.byu.edu>
2026-06-10 17:33:51 -0600

    remove tasks

--- src/myteam/tasks/resolution/__init__.py
+++
@@ -1,8 +0,0 @@
-from .reference_resolver import resolve_references
-# from .session_resolution import resolve_project_root, resolve_session_id
-from .session_resolution import resolve_session_id
-
-__all__ = [
-    "resolve_references",
-    "resolve_session_id",
-]
